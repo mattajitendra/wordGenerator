1e04688bf43f77eddb1e97f56f81ff9a6acc79e3
mattajitendra <66953300+mattajitendra@users.noreply.github.com>
2020-07-10 08:58:30 +0530

    Add files via upload
    Word generator to with UI slider to get word count

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import arrayWords from 'src/utils/words';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'word-generator';
+
+  words : string = '';
+  limit : number = 10;
+
+  handleSlideChange(newLimit: number){
+    this.limit = newLimit;
+  }
+
+
+generate(){
+  this.words = arrayWords.slice(0, this.limit).join(" ")
+}
+
+
+}
